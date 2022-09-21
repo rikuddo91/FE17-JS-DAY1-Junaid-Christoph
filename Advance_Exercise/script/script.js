223e275e96413538c5efbeafe5a537e2ae643a9b
rikuddo91 <gullkhan0@live.com>
2022-09-21 15:10:58 +0200

    Formatted

--- Advance_Exercise/script/script.js
+++ Advance_Exercise/script/script.js
@@ -6,16 +6,16 @@ let b = 14;
 
 let c = "21";
 
-let d = '36';
+let d = "36";
 
 let e = 42;
 
 // will add the numbers but consider "number" as and insert them as is.
-sum = a+b+c+d+e;
-document.write(`The sum is ${sum}.<br><br>`)
+sum = a + b + c + d + e;
+document.write(`The sum is ${sum}.<br><br>`);
 
 // will multiply the numbers but consider "number" as string and insert them as is.
-let f = '1';
+let f = "1";
 
 let g = 15;
 
@@ -23,65 +23,74 @@ let h = 8;
 
 let i = "1";
 
-multiply = f*g*h*i;
-document.write(`The result of multiplication is ${multiply}.<br><br>`)
+multiply = f * g * h * i;
+document.write(`The result of multiplication is ${multiply}.<br><br>`);
 
-division = sum/multiply;
-document.write(`The result is ${division}.<br><hr>`)
+division = sum / multiply;
+document.write(`The result is ${division}.<br><hr>`);
 
 /* ----------------------Advance Exercise 2---------------------*/
 
-const people = ['Greg', 'Mary', 'Devon','James'];
+const people = ["Greg", "Mary", "Devon", "James"];
 
 // Using shift method to remove first element
-people.shift()
-console.table(people)
+people.shift();
+console.table(people);
 
-// Using unshift method to add 'Matt' to first position 
-people.unshift("Matt")
-console.table(people)
+// Using unshift method to add 'Matt' to first position
+people.unshift("Matt");
+console.table(people);
 
 // Using Pop method to remove last element
-people.pop()
-console.table(people)
+people.pop();
+console.table(people);
 
 // Using Push method to add element at last position
-people.push("Junaid")
-console.table(people)
+people.push("Junaid");
+console.table(people);
 
-// Using slice method to removed firs two elements 
+// Using slice method to removed firs two elements
 /* people.slice(2) 
 console.table(people)
 !!! DIDN'T WORK!!!*/
 
-console.table(people.slice(2))
+console.table(people.slice(2));
 
-    // Alternate method
+// Alternate method
 // temp2 = people.slice(2);
 // console.table(temp2)
 
 // Using indexOF to find index number of "Mary"
-console.log(people.indexOf("Mary"))
+console.log(people.indexOf("Mary"));
 
 // Using indexOF to find index number of "Foo" (Doesn't exist)
-console.log(people.indexOf("Foo"))
+console.log(people.indexOf("Foo"));
 
 // Redefining variable people2 with original values
-const people2 = ['Greg', 'Mary', 'Devon','James'];
+const people2 = ["Greg", "Mary", "Devon", "James"];
 
 // Using splice to remove "Devon" and add "Elizabeth","Anna"
-people2.splice(2,1,"Elizabeth","Anna")
-console.table(people2)
+people2.splice(2, 1, "Elizabeth", "Anna");
+console.table(people2);
 
 // Adding new variable and assigning values accordingly
 withBob = people2 + ",Bob";
-console.log(withBob)
+console.log(withBob);
 
 /* ----------------------Advance Exercise 3---------------------*/
 
 // Output specific numbers from multidemensional Array - ${multi[row][col]}
-let multi = [ [1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 25, 7], [21, 4, 6, 17], [3, 5, 26, 3] ]
+let multi = [
+  [1, 2, 1, 24],
+  [8, 11, 9, 4],
+  [7, 0, 7, 27],
+  [7, 4, 28, 14],
+  [3, 10, 25, 7],
+  [21, 4, 6, 17],
+  [3, 5, 26, 3],
+];
 
-console.table(multi)
-console.log(`${multi[1][1]} ${multi[4][2]} ${multi[5][3]} ${multi[2][3]} ${multi[2][1]}`)
-
+console.table(multi);
+console.log(
+  `${multi[1][1]} ${multi[4][2]} ${multi[5][3]} ${multi[2][3]} ${multi[2][1]}`
+);
